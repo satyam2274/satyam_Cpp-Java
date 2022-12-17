42d0b3f05ae60003ecbe45dbf73271a693ef42ee
Satyam <114228515+satyam2274@users.noreply.github.com>
2022-12-17 09:25:08 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("intput.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int x;
+	cin >> x;
+	cout<<(x+10) << endl;
+	return 0;
+
+}
