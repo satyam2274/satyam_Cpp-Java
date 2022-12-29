39096da2d0c048737d6ec45e28065f2c4f22675d
Satyam <114228515+satyam2274@users.noreply.github.com>
2022-12-29 18:36:09 +0530

    Add files via upload

---
+++ Array Selection Sort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+/*
+Take as input N, the size of array. Take N more inputs and store that in an array. Write a function that selection sorts the array. Print the elements of sorted array.
+
+1.It reads a number N.
+2.Take Another N numbers as input and store them in an Array.
+3.Sort the array using Selection Sort and print that Array.
+
+Input Format
+Constraints
+N cannot be Negative. Range of Numbers can be between -1000000000 to 1000000000.
+
+Output Format
+Sample Input
+4
+2
+-18
+45
+30
+Sample Output
+-18
+2
+30
+45
+Explanation
+Write selection sort to sort the given integers in the problem.
+*/
+void SelectionSort(int arr[], int n){
+	for(int i=0; i<n-1; i++){
+        int min = i;
+		for(int j= i+1; j<n; j++){
+			if(arr[min]>arr[j]){
+				min = j;
+			}
+		}
+		swap(arr[min],arr[i]);
+	}
+}
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	SelectionSort(arr,n);
+	for(int i =0; i<n; i++){
+		cout<<arr[i]<<" ";
+	}
+
+	return 0;
+}
